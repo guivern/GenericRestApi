39e0fce765e130e7d999d214a1a2870b1c574f40
guiver <guiver95@gmail.com>
2019-07-06 20:28:43 -0400

    nombre rol es requerido

--- Models/Rol.cs
+++ Models/Rol.cs
@@ -1,7 +1,10 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace RestApiBase.Models
 {
     public class Rol: SoftDeleteEntityBase
     {
+        [Required]
         public string Nombre {get; set;}
         public string Descripcion {get; set;}
     }
