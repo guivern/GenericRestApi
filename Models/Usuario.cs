6eb8606be780bea3b570a2a02c01f3f45c7b51ae
guiver <guiver95@gmail.com>
2019-07-06 20:27:56 -0400

    code cleanup

--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -10,19 +10,27 @@ namespace RestApiBase.Models
 
         [MaxLength(UsernameMaxLength)]
         public string Username {get; set;}
+
         [JsonIgnore]
         public byte[] PasswordHash {get; set;}
+        
         [JsonIgnore]
         public byte[] PasswordSalt {get; set;}
+        
         public string Nombre {get; set;}
+        
         public string Apellido{get; set;}
+        
         public string NroDocumento {get; set;}
+        
         [JsonIgnore]
         [ForeignKey("IdRol")]
         public Rol Rol {get; set;}
         public long IdRol {get; set;}
+        
         [NotMapped]
         public virtual string NombreRol => Rol?.Nombre;
+        
         [NotMapped]
         public virtual string NombreUsuarioCreador => UsuarioCreador?.Username;
     }
