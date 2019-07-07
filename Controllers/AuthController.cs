d55073a5a72ec6dbe4597bc282e12b8ac4b087b9
guiver <guiver95@gmail.com>
2019-07-06 20:30:14 -0400

    login basado en jwt

--- Controllers/AuthController.cs
+++ Controllers/AuthController.cs
@@ -1,12 +1,53 @@
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
 using System.Security.Cryptography;
 using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using RestApiBase.Data;
+using RestApiBase.Dtos;
+using RestApiBase.Models;
 
 namespace RestApiBase.Controllers
 {
-    public class AuthController
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
     {
-        // Chequea si el password proporcionado es correcto
-        public bool IsValidPassword(string password, byte[] passwordHash, byte[] passwordSalt)
+        private readonly DataContext _context;
+        private readonly IConfiguration _configuration;
+
+        public AuthController(DataContext context, IConfiguration configuration)
+        {
+            _context = context;
+            _configuration = configuration;
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login(LoginDto dto)
+        {
+            var user = await _context.Usuarios
+            .Include(u => u.Rol)
+            .FirstOrDefaultAsync(u => u.Username.Equals(dto.Username.ToLower()));
+
+            if (user == null) return Unauthorized();
+
+            if (IsValidPassword(dto.Password, user.PasswordHash, user.PasswordSalt))
+            {
+                var token = GenerateToken(user);
+                return Ok(new { Token = token });
+            }
+
+            return Unauthorized();
+        }
+
+        private bool IsValidPassword(string password, byte[] passwordHash, byte[] passwordSalt)
         {
             using (var hmac = new HMACSHA512(passwordSalt))
             {
@@ -20,6 +61,33 @@ namespace RestApiBase.Controllers
                 }
                 return true;
             }
+        }
+
+        // Genera Jwt 
+        private string GenerateToken(Usuario user)
+        {
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Name, user.Username),
+                new Claim(ClaimTypes.Role, user.Rol.Nombre),
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration.GetSection("Jwt:Key").Value));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(Convert.ToDouble(_configuration.GetSection("Jwt:ExpireDays").Value)),
+                SigningCredentials = creds
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
         }
     }
 }
