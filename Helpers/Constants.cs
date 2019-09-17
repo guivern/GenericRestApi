495a9abf9aa715de4d3689ac6c1cc406daf0829c
guiver <guiver95@gmail.com>
2019-09-17 19:21:10 -0400

    se crea la clase constants que contendra los valores por default

---
+++ Helpers/Constants.cs
@@ -0,0 +1,9 @@
+namespace RestApiBase.Helpers
+{
+    public class Constants
+    {
+        public const int DEFAULT_PAGE_SIZE = 5;
+        public const int DEFAULT_PAGE_NUMBER = 1;
+        public const string DEFAULT_ODERING = "Id:DESC";
+    }
+}
